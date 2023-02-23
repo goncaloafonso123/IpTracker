4526a6d1d2f4275ff1aa1bd5caf572d5b3846e25
goncaloafonso123 <86926187+goncaloafonso123@users.noreply.github.com>
2023-02-23 20:47:39 +0000

    added stuff

--- src/IP_tracker.java
+++ src/IP_tracker.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,7 +11,15 @@ public class IP_tracker extends JFrame implements ActionListener {
         super("IP Finder");
         t=new JTextField();
         t.setBounds(100,100,200,20);
-        b1 =new JButton("")
+        b1 =new JButton("Search for the IP");
+        b1.setBounds(100,200,100,3);
+        b2=new JButton("Exit");
+        b2.setBounds(230,200,70,30);
+        l1= new JLabel("IP Finder");
+        l1.setBounds(120,40,300,20);
+        l1.setFont(new Font("Arial", Font.BOLD, 28));
+        b1.addActionListener(this);
+        b2.addActionListener(this);
     }
 
     public static void main(String[] args) {
