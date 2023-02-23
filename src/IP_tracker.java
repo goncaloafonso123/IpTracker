cde6ae4f49f13b57f5466fe18d93e225b758708a
goncaloafonso123 <86926187+goncaloafonso123@users.noreply.github.com>
2023-02-23 20:40:34 +0000

    Initial commit

---
+++ src/IP_tracker.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class IP_tracker extends JFrame implements ActionListener {
+    JTextField t;
+    JButton b1,b2;
+    JLabel l1;
+    public IpFinder(){
+        super("IP Finder");
+        t=new JTextField();
+        t.setBounds(100,100,200,20);
+        b1 =new JButton("")
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
